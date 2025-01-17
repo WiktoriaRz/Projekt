4065078c89890634ee785b1275edb1c9a5a0bf68
WiktoriaRz <wrzenca@st.swps.edu.pl>
2025-01-17 13:23:33 +0100

    zmiana w aplikacji

--- remiza/Psi_Patrol/admin.py
+++ remiza/Psi_Patrol/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from.models import Firefighter
+from.models import Firetruck
+from.models import Squad
+from.models import FirefighterInSquad
 
 # Register your models here.
+
+admin.site.register(Firefighter)
+admin.site.register(Firetruck)
+admin.site.register(Squad)
+admin.site.register(FirefighterInSquad)
